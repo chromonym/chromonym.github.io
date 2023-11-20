a6bef6ffb1c05a598891086cbf31a0ffc4f28028
Nick L <44630816+TheXXOs@users.noreply.github.com>
2023-11-20 17:49:31 +1030

    navigational bar
    featuring a really bad div class name pun

--- script.js
+++ script.js
@@ -15,36 +15,42 @@ function getCookie(cname) {
     return "";
 }
 
+function openMenu() {
+    document.getElementsByTagName("header")[0].classList.toggle("open");
+}
+
 var theme = "dark";
+var opened = false;
 
 window.onload = function () {
     let thm = getCookie("theme");
     if (thm == "light") {
         document.getElementById('theme_css').href = "./themes/light.css";
+        document.getElementById("theme").value = "light";
         theme = "light";
     } else if (thm == "gradient") {
         console.log("a");
         document.getElementById('theme_css').href = "./themes/gradient.css";
+        document.getElementById("theme").value = "gradient";
         theme = "gradient";
     } else {
         console.log("a");
         document.getElementById('theme_css').href = "./themes/dark.css";
+        document.getElementById("theme").value = "dark";
         theme = "dark";
     }
 };
 
-document.getElementById('themeChanger').onclick = function () {
-    if (theme == "dark") {
+document.getElementById('theme').onchange = function () {
+    theme = document.getElementById("theme").value;
+    if (theme == "light") {
         document.getElementById('theme_css').href = "./themes/light.css";
-        theme = "light";
         document.cookie = "theme=light; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;";
-    } else if (theme == "light") {
-        document.getElementById('theme_css').href = "./themes/gradient.css";
-        theme = "gradient";
-        document.cookie = "theme=gradient; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;";
     } else if (theme == "gradient") {
+        document.getElementById('theme_css').href = "./themes/gradient.css";
+        document.cookie = "theme=gradient; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;";
+    } else if (theme == "dark") {
         document.getElementById('theme_css').href = "./themes/dark.css";
-        theme = "dark";
         document.cookie = "theme=dark; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;";
     }
 }
